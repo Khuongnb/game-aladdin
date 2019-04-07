74b13ffcd623de0d6f799b0669c7e8917eef3b14
KHUONG-PC\4pro <khuongnb1997@gmail.com>
2019-04-07 16:05:11 +0700

    Completed game

---
+++ game/app/MyApp.cpp
@@ -0,0 +1,29 @@
+/*
+* Created by phuctm97 on Sep 27th 2017
+*/
+#include "MyApp.h"
+#include "MyResourceInitializer.h"
+#include "../scenes/AutoLoadScene.h"
+#include "../scenes/DebugScene.h"
+
+USING_NAMESPACE_ALA;
+
+MyApp::MyApp() : Application() {}
+
+void MyApp::onPreInitialize() {
+  // configurations
+  setResolutionSize( 320, 230 );
+  setScreenSize( 320 * 3, 230 * 3 );
+  setTitle( "Aladdin" );
+
+  setFps( 60 );
+  Logger::setLogLevel( ALA_LOG_DEBUG );
+
+  // resource initializers
+  registerResourceInitializer( new MyResourceInitializer() );
+}
+
+void MyApp::onPostInitialize() {
+  // starter scene
+  startWithScene( new AutoLoadScene( "menu.scene", false ) );
+}
